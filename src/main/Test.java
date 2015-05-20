e6a9e706391949b4a7aaa612eb79921860a19834
jkberger <jkberger@udel.edu>
2015-05-20 19:43:32 -0400

    hi

---
+++ src/main/Test.java
@@ -0,0 +1,17 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test {
+
+	a = new Car(4,Person a );
+	hashmap.add(a);
+	
+	return hashmap.length
+	}
+
+}
